15c186e2cf391db90c49f1a5a29383bcd21b14a2
GenjiWang <william20040911@gmail.com>
2021-06-09 22:58:15 +0800

    ver 3.7.1

--- Hatsune Miku.py
+++ Hatsune Miku.py
@@ -37,6 +37,11 @@ voice_lentage=0
 time_start=0
 value=1.0
 time_end=0
+vocal_hello=["D:/聲音樣本/hello2.wav","D:/聲音樣本/hello4.wav"]
+vocal_i_got_this=["D:/聲音樣本/i got this2.wav","D:/聲音樣本/i got this.wav"]
+vocal_Congratulations=["D:/聲音樣本/恭喜1.wav","D:/聲音樣本/恭喜.wav"]
+voice_channel=0
+
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data):
@@ -74,6 +79,14 @@ async def on_ready():
     change_status.start()
     print('Bot is online!')
 
+@client.event
+async def on_voice_state_update(member, before, after):
+    global queue
+    global voice_channel
+    if before.channel is not None and after.channel is None:
+
+        await voice_channel.disconnect()
+        queue = []
 
 @client.event
 async def on_message(msg):
@@ -104,7 +117,7 @@ async def ping(ctx):
 async def hello(ctx):
     if ctx.message.author.voice:
         voice = ctx.voice_client
-        voice.play(discord.FFmpegPCMAudio("D:/聲音樣本/hello2.wav"),after=lambda e: print('Player error: %s' % e) if e else None)
+        voice.play(discord.FFmpegPCMAudio(choice(vocal_hello)),after=lambda e: print('Player error: %s' % e) if e else None)
 
     else:
         await ctx.send('歐逆醬安安')
@@ -126,22 +139,23 @@ async def credits(ctx):
 
 @client.command(name='join', help='讓miku加入語音頻道')
 async def join(ctx):
+    global voice_channel
     if not ctx.message.author.voice:
         await ctx.send("你沒有加入語音頻道")
         return
     else:
-        channel = ctx.message.author.voice.channel
+        voice_channel = ctx.message.author.voice.channel
 
-    await channel.connect()
+    await voice_channel.connect()
     voice = ctx.voice_client
-    voice.play(discord.FFmpegPCMAudio("D:/聲音樣本/hello2.wav"), after=lambda e: print('Player error: %s' % e) if e else None)
+    voice.play(discord.FFmpegPCMAudio(choice(vocal_hello)), after=lambda e: print('Player error: %s' % e) if e else None)
 
 
 @client.command(name='leave', help='讓miku停止唱歌並離開語音頻道')
 async def leave(ctx):
     global queue
-    voice_client = ctx.message.guild.voice_client
-    await voice_client.disconnect()
+    voice_channel = ctx.message.guild.voice_client
+    await voice_channel.disconnect()
     queue=[]
 
 
@@ -162,6 +176,7 @@ async def loop_(ctx):
 @client.command(name='play', help='讓Miku唱歌')
 async def play(ctx):
     global queue
+    global voice_channel
     global time_start
     global voice_lentage
     global is_playing
@@ -173,10 +188,10 @@ async def play(ctx):
         return
 
     else:
-        channel = ctx.message.author.voice.channel
+        voice_channel = ctx.message.author.voice.channel
 
     try:
-        await channel.connect()
+        await voice_channel.connect()
     except:
         pass
 
@@ -269,6 +284,7 @@ async def state(ctx):
   global queue
   global loop
   global value
+
   if len(queue)==0:
     if loop==False:
       await ctx.send(f"目前還沒人點歌\n重撥模式:關閉\n目前音量{value}")
@@ -288,9 +304,27 @@ async def pause(ctx):
 
 
 @client.command(name='resume', help='讓Miku重唱一次')
-async def resume(ctx,voice_client):
+async def resume(ctx):
+    global queue
+    global is_playing
+    global voice_lentage
+    global time_start
+
+
+    server = ctx.message.guild
     voice_channel = server.voice_client
-    voice_channel.resume()
+    voice_channel.stop()
+    time.sleep(0.5)
+    player = await YTDLSource.from_url(queue[0], loop=client.loop)
+    with youtube_dl.YoutubeDL(ytdl_format_options) as ydl:
+        dictMeta = ydl.extract_info(f"{queue[0]}", download=False)
+    try:
+        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+        is_playing = True
+        voice_lentage = dictMeta['duration']
+        time_start = time.time()
+    except:
+        pass
 
 
 
@@ -303,7 +337,7 @@ async def stop(ctx):
     voice_channel.stop()
     time.sleep(0.5)
     voice = ctx.voice_client
-    voice.play(discord.FFmpegPCMAudio("D:/聲音樣本/i got this2.wav"),after=lambda e: print('Player error: %s' % e) if e else None)
+    voice.play(discord.FFmpegPCMAudio(choice(vocal_i_got_this)),after=lambda e: print('Player error: %s' % e) if e else None)
 
 
 
@@ -317,7 +351,7 @@ async def queue_(ctx, url):
         voice = ctx.voice_client
         try:
 
-            voice.play(discord.FFmpegPCMAudio("D:/聲音樣本/i got this2.wav"),after=lambda e: print('Player error: %s' % e) if e else None)
+            voice.play(discord.FFmpegPCMAudio(choice(vocal_i_got_this)),after=lambda e: print('Player error: %s' % e) if e else None)
         except:
             pass
 
@@ -368,8 +402,18 @@ async def skip(ctx):
 
 
 @client.command(name="抽卡",help="讓miku決定你是非洲人還是歐洲人")
-
 async def 抽卡(ctx):
+    global voice_channel
+    if not ctx.message.author.voice:
+        pass
+
+    else:
+        voice_channel = ctx.message.author.voice.channel
+
+    try:
+        await voice_channel.connect()
+    except:
+        pass
     random.seed(time.time())
     a = random.sample(range(1, 1000), 115)
     b = []
@@ -380,6 +424,11 @@ async def 抽卡(ctx):
     gt = random.randint(1, 1000)
     if gt in a:
         await ctx.send(f"<@{ctx.author.id}>\n" + " <:4star:828160732318138389>")
+        try:
+            voice = ctx.voice_client
+            voice.play(discord.FFmpegPCMAudio(choice(vocal_Congratulations)),after=lambda e: print('Player error: %s' % e) if e else None)
+        except:
+            pass
     elif gt in b:
         await ctx.send(f"<@{ctx.author.id}> \n" + "<:3star:828160732419063818>")
     else:
@@ -389,18 +438,17 @@ async def 抽卡(ctx):
 
 @client.command(name='十抽', help='讓miku抽十次看你是真歐洲還是真非洲')
 async def 十抽(ctx):
+    global voice_channel
     if not ctx.message.author.voice:
         pass
 
     else:
-        channel = ctx.message.author.voice.channel
+        voice_channel = ctx.message.author.voice.channel
 
     try:
-        await channel.connect()
+        await voice_channel.connect()
     except:
         pass
-
-    server = ctx.message.guild
     random.seed(time.time())
     a = random.sample(range(1, 1000), 115)
     b = []
@@ -430,7 +478,7 @@ async def 十抽(ctx):
 
         try:
             voice = ctx.voice_client
-            voice.play(discord.FFmpegPCMAudio("D:/聲音樣本/恭喜.wav"),after=lambda e: print('Player error: %s' % e) if e else None)
+            voice.play(discord.FFmpegPCMAudio(choice(vocal_Congratulations)),after=lambda e: print('Player error: %s' % e) if e else None)
         except:
             pass
 
